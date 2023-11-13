f8d27c270f0eef3778734c9ee2991388a0d94581
Rasmus Hvass Olesen <rasmush.olesen@gmail.com>
2023-11-13 18:06:08 +0100

    Tilføjet mappe (templates)
    Tilføjede mappen templates, hvor at vores html, css og javascript filer kommer til at være.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return 'This is sample text for our web solution'
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run()
