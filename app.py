4d7d40bfdf399bd1aea52bb49c90c79f1206b9ac
Rasmus Hvass Olesen <rasmush.olesen@gmail.com>
2023-11-15 20:36:12 +0100

    navigation bar

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/booking')
+def booking():
+    return render_template("booking.html")
 
 if __name__ == '__main__':
     app.run()
