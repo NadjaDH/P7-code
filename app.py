27e295566a267c4409aa391ffd54d8349994435c
Rasmus Hvass Olesen <rasmush.olesen@gmail.com>
2023-11-15 19:40:38 +0100

    Created a static folder
    CSS og javascript filer skal åbenbart være i en folder der hedder static

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template("home.html")
 
+@app.route('/book_room')
+def book_room():
+    return render_template("booking.html")
+
 if __name__ == '__main__':
     app.run()
 
-def GroupRoom
-    room_number
-    is_avaliable
-    booking_status
+#def GroupRoom()
+   # room_number
+    #is_avaliable
+    #booking_status
