9189de8862f9849f28b5eeddf945b0a67088f327
Rasmus Hvass Olesen <rasmush.olesen@gmail.com>
2023-11-16 14:23:40 +0100

    .

--- app.py
+++ app.py
@@ -5,14 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-<<<<<<< HEAD
-@app.route('/book_room')
-def book_room():
-=======
 @app.route('/booking')
 def booking():
->>>>>>> Test
-    return render_template("booking.html")
+    return render_template("BookRoom.html")
 
 if __name__ == '__main__':
     app.run()
