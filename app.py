e307b4be5516383138b464831a478a18e7dcd987
Cunosankey <146829520+Cunosankey@users.noreply.github.com>
2023-11-20 19:56:55 +0100

    se room number under fakegroup room

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from accordion import accordion_function
 import sqlite3
 from faker import Faker
 
 app = Flask(__name__)
 fake = Faker()
+
 
 @app.route('/')
 def home():
@@ -20,29 +21,40 @@ def booking():
 @app.route('/information')
 def information():
     return render_template("Information.html")
-
 @app.route('/fake_booking')
 def fake_booking():
     return fake_group_room()
 
+@app.route('/submit_booking', methods=['POST']) #this function will be called when the user clicks the submit button on the form
+def submit_booking():
+    if request.method == 'POST': #if the user has submitted the form
+        room_number = request.form.get('room') #get the room number from the form
+
+        fake_datetime = fake.date_time_this_year() #generate a fake date and time for the booking in the current year.
+
+        conn = sqlite3.connect('booking.db') #connect to the database
+        cursor = conn.cursor() #cursor is a pointer to the database. It is used to execute SQL commands
+
+        try:
+
 def fake_group_room():
-    fake_bookings = [fake.date_time() for _ in range(5)] #Generate 5 fake booking dates
-    print(fake_bookings)
+    fake_bookings = [fake.date_time_this_year() for _ in range(5)] #Generate 5 fake booking dates
     
     conn = sqlite3.connect('booking.db')
     cursor = conn.cursor() #cursor is a pointer to the database. It is used to execute SQL commands
     
     try: #try to insert the fake bookings data into the database
-        for timeslot in fake_bookings: #for each fake booking date
-            cursor.execute("INSERT INTO bookings (timeslot) VALUES (?)", (timeslot,)) 
-        conn.commit() #commit the changes to the database 
-        return  'Fake bookings succesfully added'
+        for room_number in [4118, 4119, 4120, 4121, 4122]:
+            for timeslot in fake_bookings: #for each fake booking date
+                cursor.execute("INSERT INTO bookings (timeslot) VALUES (?)", (timeslot,)) 
+            conn.commit() #commit the changes to the database 
+            return  'Fake bookings succesfully added'
     except Exception as e: #if there is an error
         print("Error adding fake bookings")
         conn.rollback() #undo the changes
         return 'Error adding the fake bookings'
     finally: #close the connection to the database
-        conn.close() #  
+        conn.close() 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == '__main__':
+        app.run(debug=True) #debug=True means that the server will reload itself each time you make a change to the code
