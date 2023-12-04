7ef0f939b93c562159e9614202fd923486124c1e
Cunosankey <146829520+Cunosankey@users.noreply.github.com>
2023-12-04 13:16:19 +0100

    1

--- accordion.py
+++ accordion.py
@@ -11,17 +11,15 @@ cursor=conn.cursor()
 cursor.execute("SELECT * FROM bookings")
 
 def accordion_function():
-    room_4118 = False
-    room_4119 = False
-    room_4120 = False
-    room_4121 = False
-    room_4122 = False
+    room_4118 = True
+    room_4119 = True
+    room_4120 = True
+    room_4121 = True
+    room_4122 = True
 
     roomnumber_list = [room_4118, room_4119, room_4120, room_4121, room_4122] # an array with the bools above
-
-    for i in range(len(roomnumber_list)):
-        roomnumber_list[i] = bool(random.getrandbits(1)) # randomly assign True or False to each room
     print(roomnumber_list)
     return roomnumber_list
+
 accordion_function()
 
