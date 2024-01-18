5648096f98ce854be980b90ecfc698994d441a44
NadjaDH <Ndha20@student.aau.dk>
2024-01-18 11:20:56 +0100

    Wihi

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from accordion import accordion_function
 import sqlite3
 from dateutil.parser import parse
 import traceback
